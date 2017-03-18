a8fd3a157adc29405485ff3748ce22f2bb7673ae
Jason Taylor <jason@jasongtaylor.com>
2017-03-18 18:35:09 -0400

    More tests

--- src/test/java/training/maven/library/example/CalculatorTest.java
+++ src/test/java/training/maven/library/example/CalculatorTest.java
@@ -10,7 +10,16 @@ public class CalculatorTest {
 
     @Test
     public void testAdd() {
-        assert true;
+        Calculator calc = new Calculator();
+        Integer sum = calc.add(2, 2);
+        assertTrue(sum == 4);
+    }
+
+    @Test
+    public void testSubtract() {
+        Calculator calc = new Calculator();
+        Integer sum = calc.subtract(6, 2);
+        assertTrue(sum == 4);
     }
 
 }
